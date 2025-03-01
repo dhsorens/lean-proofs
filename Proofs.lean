1f13bbbf0a3916e66d861ff8d7a54a3b2417a9a6
Derek Sorensen <d@dhsorens.com>
2025-03-01 09:58:29 -0500

    cleanup

--- Proofs.lean
+++ Proofs.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Proofs` library.
 -- Import modules here that should be built as part of the library.
 import Proofs.Basic
+import Proofs.Logical
+import Proofs.Quantifiers
